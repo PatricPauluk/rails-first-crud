fbdcf09a8cc8e2b3a292a963805ec1d244cd2c86
Patric Pauluk <patric.pauluk@outlook.com>
2024-09-19 20:56:18 -0300

    my first crud ror

---
+++ db/migrate/20240919231155_create_produtos.rb
@@ -0,0 +1,11 @@
+class CreateProdutos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :produtos do |t|
+      t.string :nome
+      t.text :descricao
+      t.decimal :preco
+
+      t.timestamps
+    end
+  end
+end
