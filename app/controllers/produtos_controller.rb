86c35f3f164417b552d665b6d0ba4d1206d79138
Patric Pauluk <patric.pauluk@outlook.com>
2024-09-23 21:27:02 -0300

    translated app to pt-BR

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -25,7 +25,7 @@ class ProdutosController < ApplicationController
 
     respond_to do |format|
       if @produto.save
-        format.html { redirect_to @produto, notice: "Produto was successfully created." }
+        format.html { redirect_to @produto, notice: "Produto adicionado." }
         format.json { render :show, status: :created, location: @produto }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class ProdutosController < ApplicationController
   def update
     respond_to do |format|
       if @produto.update(produto_params)
-        format.html { redirect_to @produto, notice: "Produto was successfully updated." }
+        format.html { redirect_to @produto, notice: "Produto atualizado." }
         format.json { render :show, status: :ok, location: @produto }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class ProdutosController < ApplicationController
     @produto.destroy
 
     respond_to do |format|
-      format.html { redirect_to produtos_path, status: :see_other, notice: "Produto was successfully destroyed." }
+      format.html { redirect_to produtos_path, status: :see_other, notice: "Produto excluído." }
       format.json { head :no_content }
     end
   end
